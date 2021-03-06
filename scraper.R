c6e0cf48270a528493f837cb0dd79df400880657
smach <smachlis@gmail.com>
2021-03-06 13:34:41 -0500

    Testing to see if action will run when I commit to repo

--- scraper.R
+++ scraper.R
@@ -2,17 +2,13 @@
 # https://simonwillison.net/2021/Mar/5/git-scraping/
 # used these R-specific instructions on running R with GitHub actions:
 # https://blog.rmhogervorst.nl/blog/2020/09/24/running-an-r-script-on-a-schedule-gh-actions/
+
 # and followed this file format
 # https://github.com/RMHogervorst/invertedushape/blob/main/.github/workflows/main.yml
 
-# Needed to set up:
+# Needed to set up locally:
 # remotes::install_github("smach/BostonMayor21OCPFData")
 # renv::activate()
-
-
-
-
-
 
 
 library(BostonMayor21OCPFData)
