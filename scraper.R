cd8dcecd3be52b7b009f3fd610d119f07033a1b5
smach <smachlis@gmail.com>
2021-03-06 13:18:04 -0500

    init commit

---
+++ scraper.R
@@ -0,0 +1,20 @@
+# Inspired by:
+# https://simonwillison.net/2021/Mar/5/git-scraping/
+# used these R-specific instructions on running R with GitHub actions:
+# https://blog.rmhogervorst.nl/blog/2020/09/24/running-an-r-script-on-a-schedule-gh-actions/
+# and followed this file format
+# https://github.com/RMHogervorst/invertedushape/blob/main/.github/workflows/main.yml
+
+# Needed to set up:
+# remotes::install_github("smach/BostonMayor21OCPFData")
+# renv::activate()
+
+
+
+
+
+
+
+library(BostonMayor21OCPFData)
+contributions <- purrr::map_df(the_ids, download_latest_candidate_contributions)
+data.table::fwrite(contributions, "data/contributions.csv")
